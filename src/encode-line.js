d0ff94ec66b1d21c7730ebaf405a5f781e3a8dac
alchonokk <galunia_suglob@mail.ru>
2022-10-17 14:37:36 +0300

    feat: add solution for encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let arr= str.split('');
+  // let unicArr = Array.from(new Set(arr));
+
+  let unicArr= arr.concat();
+  for (let i=0; i<unicArr.length; i++){
+    if (unicArr[i] === unicArr[i+1]){
+      unicArr.splice(i+1, 1);
+      i -= 1;
+  }}
+ 
+  let sum = 0;
+  let rez = [];
+
+  for (let i=0; i<unicArr.length; i++){
+    for (let j=0; j<arr.length; j++){
+      if (unicArr[i] === arr[j]){
+        sum += 1;
+        if (unicArr[i] !== arr[j+1]){j = arr.length}
+      }
+    }
+
+    if (sum === 0 || sum === 1){
+      rez.push(unicArr[i])
+    } else {
+      rez.push(`${sum}${unicArr[i]}`);
+    }
+    sum = 0;
+  }
+return rez.join('');
 }
 
 module.exports = {
